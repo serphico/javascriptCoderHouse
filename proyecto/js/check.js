a007dbc5c98c76173d26c8adcf01b24fabafce67
serphico <gomez.ecm@gmail.com>
2020-10-04 21:58:51 -0300

    realizado check y uncheck de imagenes

--- proyecto/js/check.js
+++ proyecto/js/check.js
@@ -1,11 +1,11 @@
 let checkImgRealizado = document.getElementsByClassName("imgProductoStock");
-let checkRealizado = document.getElementsByClassName("productoRealizado");
+let checkRealizado = document.document.getElementsByTagName(
+  "productoRealizado"
+);
 
 for (let i = 0; i < checkImgRealizado.length; i++) {
   checkImgRealizado[i].addEventListener("click", function mostrarValor() {
-    checkRealizado[0].checked = false;
-    checkRealizado[1].checked = false;
-    checkRealizado[2].checked = false;
+    checkRealizado[i].checked = false;
   });
 }
 
